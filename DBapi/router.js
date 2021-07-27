8f0ce1086295f9805fafa21ca50724d7a883faee
AndieMadd <67398879+AndieMadd@users.noreply.github.com>
2021-07-27 14:51:25 +1200

    Added Reply Feature

--- DBapi/router.js
+++ DBapi/router.js
@@ -6,7 +6,7 @@ const { create } = require('domain');
 
 
 
-const client = new OAuth2Client("897864354621-rqs8e9er3647tn3g6d2n0ecmulfkn28f.apps.googleusercontent.com");
+const client = new OAuth2Client("897864354621-el118c9a3qd729f7joaad2fitj5f4t8e.apps.googleusercontent.com");
 const router = express.Router()
 let db
 
@@ -17,7 +17,7 @@ Database.open(path.join(__dirname, 'database', 'database.db')).then(newDB => db 
 async function verify(token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
-        audience: "897864354621-rqs8e9er3647tn3g6d2n0ecmulfkn28f.apps.googleusercontent.com",
+        audience: "897864354621-el118c9a3qd729f7joaad2fitj5f4t8e.apps.googleusercontent.com",
     });
     const payload = ticket.getPayload();
     return {
@@ -39,9 +39,17 @@ router.post('/forums', async (req, res) => {
 //Gets all the posts from a specified forum.
 router.post('/forum/:tok', async (req, res) => {
     const token = req.params.tok
-    let { id } = await db.get(`select id from forums where token = ?`, [token])
-    let posts = await db.all(`select posts.*, users.name from posts inner join users on posts.id_users = users.id where posts.id_forums = ?`, [id])
+    let id = await db.get(`select id from forums where token = ?`, [token])
+    let posts = await db.all(`select posts.*, users.name from posts inner join users on posts.id_users = users.id where posts.id_forums = ?`, [id.id])
     res.json(posts)
+})
+
+router.post('/post/:id', async (req, res) => {
+    const postID = req.params.id
+    //"select replies.*, users.name from replues inner join users on replies.id_users = users.id where replies.id_posts = ?", [postID]
+    let replies = await db.all("select replies.*, users.name from replies inner join users on replies.id_users = users.id where replies.id_posts = ?", [postID])
+    console.log(replies);
+    res.json(replies)
 })
 
 router.post('/login', async (req, res) => {
@@ -60,12 +68,23 @@ router.post('/login', async (req, res) => {
     return res.status(200).json({ name: user.name, pfp: user.pfp_url })
 })
 
+
 router.post('/addPost', async (req, res) => {
     const { title, content, forum } = req.body
     const userId = req.session.userID
     const timeCreated = Date.now()
     const { id } = await db.get('select id from forums where token = ?', [forum])
     await db.run('insert into posts (title, content, id_forums, id_users, timeCreated) values(?,?,?,?,?)', [title, content, id, userId, timeCreated])
+    res.status(204).json({})
+})
+
+router.post('/addReply', async (req, res) => {
+    console.log(req.body);
+    const { content, post } = req.body
+    const userId = req.session.userID
+    console.log(userId, content, post);
+    //"insert into replies (content, id_posts, id_users) values(?,?,?)", [content, postID, userID]
+    await db.run("insert into replies (content, id_posts, id_users) values(?,?,?)", [content, post, userId])
     res.status(204).json({})
 })
 
